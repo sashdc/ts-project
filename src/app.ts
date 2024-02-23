b28f44141622296c1d778d6935062e7bdd796671
sashdc <saharshdc@gmail.com>
2024-02-23 10:26:01 -0500

    added rendering projects with details

--- src/app.ts
+++ src/app.ts
@@ -130,9 +130,9 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     newElementID?: string
   ) {
     this.templateElement = document.getElementById(
-      "project-list"
+      templateId
     )! as HTMLTemplateElement;
-    this.hostElement = document.getElementById("app")! as T;
+    this.hostElement = document.getElementById(hostElementId)! as T;
 
     const importedNode = document.importNode(
       this.templateElement.content,
@@ -156,6 +156,25 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+//  ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+    this.configure();
+    this.renderContent();
+  }
+
+  configure(){}
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -165,7 +184,6 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 
     this.assignedProjects = [];
 
-    // this.element.id = `${this.type}-projects`;
 
     this.configure();
     this.renderContent();
@@ -184,7 +202,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     });
   }
 
-  private renderContent() {
+   renderContent() {
     const listId = `${this.type}-projects-list`;
     this.element.querySelector("ul")!.id = listId;
     this.element.querySelector("h2")!.textContent =
@@ -197,9 +215,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     )! as HTMLUListElement;
     listEl.innerHTML = "";
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+     new ProjectItem(this.element.querySelector('ul')!.id, prjItem)
     }
   }
 }
